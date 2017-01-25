56c6ab7e82681924dd5fc544280bd6c89d796482
Andre <dre.board@gmail.com>
2017-01-25 01:18:28 -0500

    Sample sql files

---
+++ assets/users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `age` int(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8
+
+INSERT INTO `users` (`name`, `email`, `age`) VALUES
+('Sonny Corleone', 'sonny@gmail.com', '29');
+
+INSERT INTO `users` (`name`, `email`, `age`) VALUES
+('Tony Montana', 'scar@gmail.com', '39');
+
+INSERT INTO `users` (`name`, `email`, `age`) VALUES
+('Billy Batts', 'batts@gmail.com', '57');
