607f448279ee78d68fe20b00d829518f15a4cb71
andreboard <dre.board@gmail.com>
2017-07-03 13:21:29 -0400

    components

--- components/GitTags.php
+++ components/GitTags.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Components;
 
 /**
@@ -7,42 +8,41 @@ namespace App\Components;
  * @since       v0.1.0
  *
  */
-abstract class GitTags {
+abstract class GitTags
+{
 
-	const MAJOR = 1;
-	const MINOR = 2;
-	const PATCH = 3;
+    const MAJOR = 1;
+    const MINOR = 2;
+    const PATCH = 3;
 
-	public static function get()
-	{
-		$commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+    public static function get()
+    {
+        $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
 
-		$commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
-		$commitDate->setTimezone(new \DateTimeZone('UTC'));
+        $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
+        $commitDate->setTimezone(new \DateTimeZone('UTC'));
 
-		return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
-	}
+        return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:m:s'));
+    }
 
-	/**
-	 *
-	 */
-	public static function getCurrentTag()
-	{
-		$versions = glob('../.git/refs/tags/*');
-		natsort($versions);
-		$latest = array_pop($versions);
-		$latest = basename($latest);
-		$components = explode('.', $latest);
-		$version = [
-			'whole' => $latest,
-			'major' => (int)$components[0],
-			'minor' => $components[1],
-			'release' => $components[2]
-		];
+    /**
+     * Get the current tag from git
+     */
+    public static function getCurrentTag()
+    {
+        $versions = glob('../.git/refs/tags/*');
+        natsort($versions);
+        $latest = array_pop($versions);
+        $latest = basename($latest);
+        $components = explode('.', $latest);
+        $version = [
+            'whole' => $latest,
+            'major' => (int)$components[0],
+            'minor' => $components[1],
+            'release' => $components[2]
+        ];
 
-		return $version;
-	}
+        return $version;
+    }
 
 }
-
-//echo GitTags::getCurrentTag();
