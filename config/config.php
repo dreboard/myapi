867d0851163d4dd3ffe0401ef19877f3d5510327
Andre <dre.board@gmail.com>
2017-08-23 21:54:59 -0400

    server env

--- config/config.php
+++ config/config.php
@@ -10,6 +10,5 @@
 $path = '../vendor/autoload.php';
 $settings_path = '../src/';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path . PATH_SEPARATOR . $settings_path);
-apache_setenv('APPLICATION_ENV','development');
 ini_set('date.timezone', 'America/Chicago');
 
